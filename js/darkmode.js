8ea218c38e8f544f2fd73b712c69e36db5981d51
Daniel <danielarturo.marquez@gmail.com>
2020-05-25 19:52:36 -0400

    se agregar darkmode

---
+++ js/darkmode.js
@@ -0,0 +1,18 @@
+function darky(){
+    var darkmode = document.getElementById('dark');
+    
+    if (darkmode.checked == true) {
+        document.getElementById("css").href = 'css/estilo-dark.css';
+    } else {
+        document.getElementById("css").href = 'css/estilo.css';
+    }
+}
+
+window.onload = function () {
+    darky();
+    document.addEventListener('click', function(e){
+        if (event.target.nodeName == 'INPUT' && event.target.type == 'checkbox') {
+            darky();
+        }
+    });
+}
